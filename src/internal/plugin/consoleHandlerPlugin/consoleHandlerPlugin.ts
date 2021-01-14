1e266605fba3c2ebb107b7d08dbc4efc3c08d79f
Ivan Novikov <ivan7237d@gmail.com>
2021-01-14 11:41:42 -0500

    feat: prevent confusing badge numbers when using with React dev mode

--- src/internal/plugin/consoleHandlerPlugin/consoleHandlerPlugin.ts
+++ src/internal/plugin/consoleHandlerPlugin/consoleHandlerPlugin.ts
@@ -1,8 +1,15 @@
-import { asNever } from 'antiutils';
+import { applyPipe, asNever } from 'antiutils';
 import { GlobalPlugin, globalPluginSymbol } from '../../logger/plugin';
 import { SeverityLevel } from '../../logger/severityLevel';
 import { detectedLogStyle } from './detectedLogStyle';
 import { pureConsoleHandler } from './pureConsoleHandler/pureConsoleHandler';
+
+// This works around the problem that React overwrites console methods in dev
+// mode during test renders, leading to confusing badge numbers.
+const consoleSnapshot = applyPipe(
+  console,
+  ({ debug, info, warn, error, log }) => ({ debug, info, warn, error, log }),
+);
 
 /**
  * A plugin that writes log messages to the console.
@@ -12,15 +19,15 @@ export const consoleHandlerPlugin: GlobalPlugin = {
   handler: pureConsoleHandler({
     getImpureHandler: (severityLevel) =>
       severityLevel === SeverityLevel.debug
-        ? console.debug
+        ? consoleSnapshot.debug
         : severityLevel === SeverityLevel.info
-        ? console.info
+        ? consoleSnapshot.info
         : severityLevel === SeverityLevel.warn
-        ? console.warn
+        ? consoleSnapshot.warn
         : severityLevel === SeverityLevel.error
-        ? console.error
+        ? consoleSnapshot.error
         : severityLevel === undefined
-        ? console.log
+        ? consoleSnapshot.log
         : asNever(severityLevel),
     logStyle: detectedLogStyle,
     maxLength: 4000,
