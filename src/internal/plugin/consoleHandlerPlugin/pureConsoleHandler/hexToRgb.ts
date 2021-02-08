19b4535d3fe0bad127e9a644b4998a95cb35883d
Ivan Novikov <ivan7237d@gmail.com>
2021-02-08 13:09:05 -0500

    refactor: remove unused branch

--- src/internal/plugin/consoleHandlerPlugin/pureConsoleHandler/hexToRgb.ts
+++ src/internal/plugin/consoleHandlerPlugin/pureConsoleHandler/hexToRgb.ts
@@ -6,7 +6,8 @@ import { applyPipe } from 'antiutils';
 export const hexToRgb = (source: string): [number, number, number] =>
   applyPipe(
     source.startsWith('#') ? source.substring(1).match(/.{2}/g) : [],
-    (value) => (value ?? []).map((value) => parseInt(value, 16)),
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    (value) => value!.map((value) => parseInt(value, 16)),
     (value) => {
       if (value.length === 3 && value.every(Number.isFinite)) {
         // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
