3e99bd153ea23aff53f9622fe2bdca44f0cce560
Ivan Novikov <ivan7237d@gmail.com>
2021-02-19 21:14:40 -0500

    fix: potential issue with some bundlers detecting Node's require

--- src/internal/logger/timeDelta.ts
+++ src/internal/logger/timeDelta.ts
@@ -8,9 +8,18 @@ let timeDelta = 0;
 if (typeof performance === 'undefined') {
   // Polyfill `performance` for Node. We can't use a poNyfill because we need to
   // leave to the user ability to stub out the `performance` object for the
-  // purposes of unit tests. We use eval to prevent Webpack from noticing this
-  // import.
-  global.performance = eval('require')('perf_hooks').performance;
+  // purposes of unit tests.
+
+  // Below code that reads `require` copied from
+  // https://github.com/facebook/react/blob/8af27aeedbc6b00bc2ef49729fc84f116c70a27c/packages/shared/enqueueTask.js#L16-L19
+
+  // read require off the module object to get around the bundlers.
+  // we don't want them to detect a require and bundle a Node polyfill.
+  const requireString = ('require' + Math.random()).slice(0, 7);
+  // eslint-disable-next-line @typescript-eslint/consistent-type-assertions, @typescript-eslint/no-explicit-any
+  const nodeRequire = module && (module as any)[requireString];
+
+  global.performance = nodeRequire('perf_hooks').performance;
 }
 
 /**
