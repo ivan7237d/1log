5bc92d653efa46a7334a13149ba1e0777185706f
Ivan Novikov <ivan7237d@gmail.com>
2022-12-27 17:00:30 -0500

    test github action

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,6 +3,6 @@ REPO_VERSION=$(pnpm pkg get version | sed 's/"//g')
 PUBLISHED_VERSION=$(pnpm view "$PNPM_PACKAGE_NAME" version)
 if [ $REPO_VERSION != "$PUBLISHED_VERSION" ]
   then
-    pnpm publish --publish-branch=master --access=public || exit 1
+    pnpm publish --access=public || exit 1
     git tag $PNPM_PACKAGE_NAME@$REPO_VERSION
 fi
