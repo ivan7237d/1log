f398a8ec2dbaff7b70fc46f3592befe2aeb002dd
Ivan Novikov <ivan7237d@gmail.com>
2022-12-27 16:56:58 -0500

    test github action

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,9 +1,8 @@
 REPO_VERSION=$(pnpm pkg get version | sed 's/"//g')
 # PUBLISHED_VERSION will be blank if the package is not published yet (404).
 PUBLISHED_VERSION=$(pnpm view "$PNPM_PACKAGE_NAME" version)
-git diff
 if [ $REPO_VERSION != "$PUBLISHED_VERSION" ]
   then
-    pnpm publish --access=public || exit 1
+    pnpm publish --publish-branch=master --access=public || exit 1
     git tag $PNPM_PACKAGE_NAME@$REPO_VERSION
 fi
